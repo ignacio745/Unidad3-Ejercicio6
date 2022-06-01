d19f6af70774932cc9c7d948daed69c437337349
ignacio745 <62523458+ignacio745@users.noreply.github.com>
2022-06-01 10:29:41 -0300

    Delete ErrorIndice.py

--- ErrorIndice.py
+++
@@ -1,9 +0,0 @@
-class ErrorIndice(IndexError):
-    __mensaje = ""
-
-    def __init__(self, *args: object) -> None:
-        """
-        Excepcion para indices fuera de rango en la interfaz IColeccion
-        """
-        
-        super().__init__(*args)
